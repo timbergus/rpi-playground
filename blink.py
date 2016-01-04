75e0ec94931b9f69cb9c3579b197096193647255
Gustavo Muñoz <timbergus@gmail.com>
2016-01-04 04:41:12 +0100

    Changes in blink.

--- blink.py
+++ blink.py
@@ -8,13 +8,14 @@ def Blink(numTimes, speed):
     for i in range(0, numTimes):
         print "Iteration " + str(i + 1)
         GPIO.output(7, True)
-        time.sleep(speed)
+        time.sleep(speed / 1000)
         GPIO.output(7, False)
-        time.sleep(speed)
+        time.sleep(speed / 1000)
     print "Done"
     GPIO.cleanup()
 
-iterations = raw_input("Enter total number of times to blink: ")
-speed = raw_input("Enter length of each blink: ")
+iterations = raw_input("Enter total number of blinks: ")
+speed = raw_input("Enter blinking time: ")
 
 Blink(int(iterations), float(speed))
+
