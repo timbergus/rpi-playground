57566a659f13e0644012b0d30e40bae190eddcb7
Gustavo Muñoz <timbergus@gmail.com>
2016-03-04 23:31:57 +0100

    Camera and server

--- python-server/index.py
+++ python-server/index.py
@@ -1,21 +1,42 @@
+#import RPi.GPIO as GPIO
+import picamera
+
+camera=picamera.PiCamera()
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(16, GPIO.OUT)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #camera.capture('image.jpg')
+    return render_template('index.html', image='image.jpg')
 
 
-@app.route('/cakes')
-def cakes():
-    return 'Yummy cakes!'
+#@app.route('/cakes')
+#def cakes():
+#    return 'Yummy cakes!'
 
 
-@app.route('/hello/<name>')
-def hello(name):
-    return render_template('page.html', name=name)
+#@app.route('/hello/<name>')
+#def hello(name):
+#    return render_template('page.html', name=name)
+
+
+#@app.route('/led/<state>')
+#def led(state):
+#    if state == 'on':
+        # GPIO.output(16, True)
+#        return 'Led ON!'
+#    else:
+        # GPIO.output(16, False)
+#        return 'Led OFF!'
+
+
 
 
 if __name__ == '__main__':
