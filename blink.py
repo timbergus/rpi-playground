1d14d4fb4d804aca8e02b5dcf2c3f1e8838def91
Gustavo Muñoz <timbergus@gmail.com>
2016-03-05 20:11:51 +0100

    Cool blinking :)

--- blink.py
+++ blink.py
@@ -1,15 +1,34 @@
 import RPi.GPIO as GPIO
 import time
+import random
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(33, GPIO.OUT) # blue
+GPIO.setup(35, GPIO.OUT) # green
+GPIO.setup(37, GPIO.OUT) # blue
+
+def red(status):
+    GPIO.output(33, status)
+
+def green(status):
+    GPIO.output(35, status)
+
+def blue(status):
+    GPIO.output(37, status)
+def ledON(led):
+    GPIO.output(led, True)
+
+def ledOFF(led):
+    GPIO.output(led, False)
+
+leds = [33, 35, 37]
 
 def Blink(numTimes, speed):
     for i in range(0, numTimes):
         print "Iteration " + str(i + 1)
-        GPIO.output(7, True)
+        ledON(random.choice(leds))
         time.sleep(speed / 1000)
-        GPIO.output(7, False)
+        ledOFF(random.choice(leds))
         time.sleep(speed / 1000)
     print "Done"
     GPIO.cleanup()
