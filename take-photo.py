4e0fc6525a2684304c086e9030dc8b2b3b81f767
Gustavo Muñoz <timbergus@gmail.com>
2016-05-29 23:46:37 +0200

    Preview before taking picture to adjust camera.

--- take-photo.py
+++ take-photo.py
@@ -1,5 +1,9 @@
-import picamera
+from picamera import PiCamera
+from time import sleep
 
-camera = picamera.PiCamera()
+camera = PiCamera()
 
+camera.start_preview()
+sleep(5)
 camera.capture('image.jpg')
+camera.stop_preview()
