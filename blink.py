2756db583201f183163c70f418c91681108e0453
Gustavo Muñoz <timbergus@gmail.com>
2017-12-23 20:38:25 +0100

    Fix bugs.

--- blink.py
+++ blink.py
@@ -21,6 +21,15 @@ def green(status):
 def blue(status):
     """Set blue color output."""
     GPIO.output(37, status)
+
+def led_on(led):
+    """Turn LED on."""
+    GPIO.output(led, True)
+
+def led_off(led):
+    """Turn LED off."""
+    GPIO.output(led, False)
+
 def blink(iterations, speed):
     """Makes the led blink."""
     leds = [33, 35, 37]
@@ -45,9 +54,10 @@ def blink(iterations, speed):
     GPIO.cleanup()
 
 def main():
-    ITERATIONS = input("Enter total number of blinks: ")
-    SPEED = input("Enter blinking time: ")
-    blink(int(ITERATIONS), float(SPEED))
+    """MAIN"""
+    iterations = input("Enter total number of blinks: ")
+    speed = input("Enter blinking time: ")
+    blink(int(iterations), float(speed))
 
 if __name__ == "__main__":
     main()
