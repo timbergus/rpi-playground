f746924efef4f1a2011f3a147dfc52511a2ede94
Gustavo Muñoz <timbergus@gmail.com>
2018-11-09 22:14:16 +0100

    Add folder for images.

--- hardware/take_photo.py
+++ hardware/take_photo.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3.4
-"""Let's blink!."""
+"""Let's take a photo!."""
 
+from os import rename, path, listdir
+from shutil import move
 from picamera import PiCamera
 from time import sleep
 
+DIR = 'images'
 camera = PiCamera()
 
 camera.start_preview()
 sleep(5)
 camera.capture('image.jpg')
 camera.stop_preview()
+
+move('image.jpg', 'images/image' + str(len(listdir(DIR)))  + '.jpg')
